98a2f5d33f4e75d1052a36892643c110cc2f46dd
Steve Johnson <steve21johnson@gmail.com>
2019-02-22 14:51:34 -0800

    organize code

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,23 +1,9 @@
-//toggle completion
-$("#list").on("click", ".todo", function() {
-  $(this).toggleClass("completed");
-});
-
-//delete
-$("#list").on("click", ".fa-trash-alt", function(event) {
-  $(this)
-    .parent()
-    .fadeOut(500, function() {
-      $(this).remove();
-    });
-  event.stopPropogation();
-});
-
-//show hide add new todo
+//show hide add new todo input
 $(".fa-plus").click(function() {
   $("input").fadeToggle();
 });
 
+//handle new todo input
 $("input").keypress(function(event) {
   if (event.which === 13) {
     $("#list").append(
@@ -27,4 +13,19 @@ $("input").keypress(function(event) {
     );
     $(this).val("");
   }
+});
+
+//toggle todo completion
+$("#list").on("click", ".todo", function() {
+  $(this).toggleClass("completed");
+});
+
+//delete todo
+$("#list").on("click", ".fa-trash-alt", function(event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
+    });
+  event.stopPropogation();
 });
