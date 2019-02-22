eb3be9ee989a78db3de931296b6aa6d636f383fb
Steve Johnson <steve21johnson@gmail.com>
2019-02-22 14:34:08 -0800

    style

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -13,80 +13,18 @@ $("#list").on("click", ".fa-trash-alt", function(event) {
   event.stopPropogation();
 });
 
-// $(".fa-trash-alt").click(function() {
-//   $(this)
-//     .parent()
-//     .fadeOut(500, function() {
-//       $(this).remove();
-//     });
-// });
-
-// $("div.todo").mouseover(function() {
-//   console.log("in");
-//   $(this)
-//     .children("i")
-//     .show("fast");
-// });
-
-// $("div.todo").mouseout(function() {
-//   console.log("out");
-//   $(this)
-//     .children("i")
-//     .hide("fast");
-// });
-
 //show hide add new todo
 $(".fa-plus").click(function() {
-  $("input").toggle();
+  $("input").fadeToggle();
 });
 
 $("input").keypress(function(event) {
   if (event.which === 13) {
     $("#list").append(
-      "<div class='todo'><i class='fas fa-trash-alt'></i>" +
+      "<div class='todo'><i class='fas fa-trash-alt'></i><span> " +
         $(this).val() +
-        "</div>"
+        "</span></div>"
     );
     $(this).val("");
   }
 });
-
-//add new todo and add listeners
-// $("input").keypress(function(e) {
-//   if (e.which == 13) {
-//     //create new item
-//     $("div#list").append(
-//       "<div class='todo'><i class='fas fa-trash-alt'></i>" +
-//         e.target.value +
-//         "</div>"
-//     );
-//     $("div.todo")
-//       .last()
-//       .on("click", function() {
-//         $(this).toggleClass("completed");
-//       });
-//     $("div.todo")
-//       .last()
-//       .on("mouseover", function() {
-//         $(this)
-//           .children()
-//           .show("fast");
-//       });
-//     $("div.todo")
-//       .last()
-//       .on("mouseout", function() {
-//         $(this)
-//           .children()
-//           .hide("fast");
-//       });
-//     $(".fa-trash-alt")
-//       .last()
-//       .click(function() {
-//         console.log($(this));
-//         $(this)
-//           .parent()
-//           .remove();
-//       });
-//     e.target.value = "";
-//   }
-// });
